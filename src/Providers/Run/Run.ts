42685630c381e4cc3f2c628708cbfd9f0b829a2e
Jason Kuhrt <jasonkuhrt@me.com>
2021-09-17 16:20:17 -0400

    do not assume npm

--- src/Providers/Run/Run.ts
+++ src/Providers/Run/Run.ts
@@ -2,24 +2,34 @@ import * as Execa from 'execa'
 import { createDynamicProvider, DynamicProvider } from '../../kont'
 import { Dir } from '../Dir'
 
+export type Params = {
+  /**
+   * Which package manager should `runPackageScript` use?
+   *
+   * @default `'npm'`
+   */
+  packageManager?: 'npm' | 'yarn' | 'pnpm'
+}
+
 export type Needs = Partial<Dir.Contributes>
 
 export type Contributes = {
   run(command: string, options?: Execa.SyncOptions): Execa.ExecaSyncReturnValue
-  runNpmScript(command: string, options?: Execa.SyncOptions): Execa.ExecaSyncReturnValue
+  runPackageScript(command: string, options?: Execa.SyncOptions): Execa.ExecaSyncReturnValue
   runAsync(command: string, options?: Execa.SyncOptions): Execa.ExecaChildProcess
   runOrThrow(command: string, options?: Execa.SyncOptions): Execa.ExecaSyncReturnValue
-  runOrThrowNpmScript(command: string, options?: Execa.SyncOptions): Execa.ExecaSyncReturnValue
+  runOrThrowPackageScript(command: string, options?: Execa.SyncOptions): Execa.ExecaSyncReturnValue
 }
 
 /**
  *  If upstream adds `fs` to context then is used to get the default CWD for commands.
  */
-export const run = (): DynamicProvider<Needs, Contributes> =>
+export const run = (params: Params): DynamicProvider<Needs, Contributes> =>
   createDynamicProvider<Needs, Contributes>((register) =>
     register.before((ctx) => {
       const cwd = ctx.fs?.cwd() ?? process.cwd()
-      return {
+      const packageManager = params.packageManager ?? 'npm'
+      const api: Contributes = {
         run(command, options) {
           // console.log(`${command} ...`)
           return Execa.commandSync(command, {
@@ -28,9 +38,9 @@ export const run = (): DynamicProvider<Needs, Contributes> =>
             reject: false,
           })
         },
-        runNpmScript(command, options) {
+        runPackageScript(command, options) {
           // console.log(`${command} ...`)
-          return Execa.commandSync(`npm run --silent ${command}`, {
+          return Execa.commandSync(`${packageManager} run --silent ${command}`, {
             cwd,
             ...options,
             reject: false,
@@ -43,9 +53,9 @@ export const run = (): DynamicProvider<Needs, Contributes> =>
             ...options,
           })
         },
-        runOrThrowNpmScript(command, options) {
+        runOrThrowPackageScript(command, options) {
           // console.log(`${command} ...`)
-          return Execa.commandSync(`npm run --silent ${command}`, {
+          return Execa.commandSync(`${packageManager} run --silent ${command}`, {
             cwd,
             ...options,
           })
@@ -58,5 +68,7 @@ export const run = (): DynamicProvider<Needs, Contributes> =>
           })
         },
       }
+
+      return api
     })
   )
--- src/Providers/index_.ts
+++ src/Providers/index_.ts
@@ -1,1 +1,2 @@
 export * from './Dir'
+export * from './Run'
